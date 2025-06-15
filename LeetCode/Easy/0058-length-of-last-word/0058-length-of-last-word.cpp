8daad660cc7d4ba53de0adcd66defbe926c940f9
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2025-06-15 09:58:27 +0530

    Time: 0 ms (100%), Space: 8 MB (99.98%) - LeetHub

---
+++ LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int lens = 0;
+        int i = s.size() - 1;
+        while (i >= 0 && s[i] == ' ')
+            i--;
+        while (i >= 0 && s[i] != ' ') {
+            lens++;
+            i--;
+        }
+        return lens;
+    }
+};
