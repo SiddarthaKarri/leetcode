fc4b6df3b7c864b205d39761327e04bb0496aa9b
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2025-07-04 08:23:02 +0530

    Time: 12 ms (12.25%), Space: 78.8 MB (8.13%) - LeetHub

---
+++ LeetCode/Medium/0848-shifting-letters/0848-shifting-letters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char shift(char c, long long x=1) {
+        return 'a'+((c-'a'+x)%26);
+    }
+    string shiftingLetters(string s, vector<int>& shifts) {
+        string res;
+        vector<long long> prefix(s.length(),0);
+        long long sum=0;
+        for(int i=0;i<shifts.size();i++)
+            sum += shifts[i];
+        prefix[0] = sum;
+        for(int i=1;i<shifts.size();i++)
+            prefix[i] = (prefix[i-1]-shifts[i-1]);
+        for(int i=0;i<s.length();i++){
+            char c = shift(s[i],prefix[i]);
+            res+=c;
+        }
+        return res;
+    }
+};
