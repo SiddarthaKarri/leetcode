b6087dbf3cb391cc2fe0194aef19f55800c2977d
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2025-08-19 21:53:53 +0530

    Time: 0 ms (100%), Space: 111.2 MB (95.09%) - LeetHub

--- LeetCode/Medium/2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ LeetCode/Medium/2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long ans = 0;
-        long long cnt =0,maxi=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
+        long long ans = 0,cnt =0;
+        for(int i:nums){
+            if(i==0){
                 cnt++;
-                if(i==nums.size()-1)
-                    ans += ((cnt*(cnt+1))/2);
-            } else{
-                ans += ((cnt*(cnt+1))/2);
+                ans += cnt;
+            }else{
                 cnt = 0;
             }
         }
