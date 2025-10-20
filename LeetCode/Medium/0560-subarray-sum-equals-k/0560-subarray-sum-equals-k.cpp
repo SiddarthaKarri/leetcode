cd7db7621ffdd236a0ba969ae9dd01cbbdf1c75e
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2025-10-20 09:10:27 +0530

    Time: 51 ms (49.29%), Space: 50.9 MB (20.93%) - LeetHub

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,17 +1,31 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         unordered_map<long long, int> preSumMap;
+//         preSumMap[0] = 1;
+//         long long sum = 0;
+//         int cnt = 0;
+//         for(int i=0;i<n;i++){
+//             sum += nums[i];
+//             long long rem = sum-k;
+//             if(preSumMap.find(rem) != preSumMap.end())
+//                 cnt += preSumMap[rem];
+//             preSumMap[sum]++;
+//         }
+//         return cnt;
+//     }
+// };
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<long long, int> preSumMap;
-        preSumMap[0] = 1;
-        long long sum = 0;
-        int cnt = 0;
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-            long long rem = sum-k;
-            if(preSumMap.find(rem) != preSumMap.end())
-                cnt += preSumMap[rem];
-            preSumMap[sum]++;
+        unordered_map<int,int> pre;
+        pre[0] = 1;
+        int sum=0, cnt=0;
+        for(int num:nums){
+            sum += num;
+            cnt += pre[sum-k];
+            pre[sum]++;
         }
         return cnt;
     }
