e086f0a8f9c9e4b3f86fdb60c67f1c5d04f136f8
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-07 22:48:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.cpp
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i+=3){
-            if(nums[i-1]!=nums[i])
-                return nums[i-1];
+        int ones = 0, twos = 0;
+        for(int num:nums){
+            ones = (ones^num) & ~twos;
+            twos = (twos^num) & ~ones;
         }
-        return nums[nums.size()-1];
+        return ones;
+        // sort(nums.begin(),nums.end());
+        // for(int i=1;i<nums.size();i+=3){
+        //     if(nums[i-1]!=nums[i])
+        //         return nums[i-1];
+        // }
+        // return nums[nums.size()-1];
         // int res = 0;
         // for(int i=0;i<32;i++){
         //     int sum = 0;
