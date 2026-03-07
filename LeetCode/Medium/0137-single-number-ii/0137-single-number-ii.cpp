a2a94caf84e2c6f590c836c89d213ae2bb9f7174
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-07 22:38:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.cpp
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res = 0;
-        for(int i=0;i<32;i++){
-            int sum = 0;
-            for(int num:nums)
-                if(num & (1<<i))
-                    sum++;
-            if(sum%3==1)
-                res |= (1 << i);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]==nums[i] && nums[i]==nums[i+1])
+                continue;
+            else
+                return i-1;
         }
-        return res;
+        return -1;
+        // int res = 0;
+        // for(int i=0;i<32;i++){
+        //     int sum = 0;
+        //     for(int num:nums)
+        //         if(num & (1<<i))
+        //             sum++;
+        //     if(sum%3==1)
+        //         res |= (1 << i);
+        // }
+        // return res;
     }
 };
