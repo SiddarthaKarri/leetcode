f9f2ab87df2f710de9fc713fd0c0adb11397135b
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-22 22:19:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.cpp
+++ LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int help(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
-        if(ind==0)
-            return (T%arr[0]==0);
-        if(dp[ind][T]!=-1)
-            return dp[ind][T];
-        int notpic = help(arr,ind-1,T,dp),pic = 0;
-        if(arr[ind]<=T)
-            pic = help(arr,ind,T-arr[ind],dp);
-        return dp[ind][T] = notpic + pic;
-    }
+    // memo
+    // int help(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
+    //     if(ind==0)
+    //         return (T%arr[0]==0);
+    //     if(dp[ind][T]!=-1)
+    //         return dp[ind][T];
+    //     int notpic = help(arr,ind-1,T,dp),pic = 0;
+    //     if(arr[ind]<=T)
+    //         pic = help(arr,ind,T-arr[ind],dp);
+    //     return dp[ind][T] = notpic + pic;
+    // }
 
     int change(int amount, vector<int>& coins) {
         // vector<unsigned long int> dp(amount+1,0);
@@ -19,7 +20,22 @@ public:
         //         dp[i] += dp[i-c];
         // return dp[amount];
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        return help(coins,n-1,amount,dp);
+        // vector<vector<int>> dp(n, vector<int>(amount+1,-1));
+        // return help(coins,n-1,amount,dp);
+
+        // tabulation
+        vector<vector<int>> dp(n, vector<int>(T + 1, 0));
+        for(int i=0;i<=amount;i++)
+            if(i%arr[0]==0)
+                dp[0][i] = 1;
+        for(int ind=1;ind<n;ind++){
+            for(int tar=0;tar<=amount;tar++){
+                int notpic = dp[ind-1][tar],pic=0;
+                if(arr[ind]<=tar)
+                    pic = dp[ind][tar-arr[ind]];
+                dp[ind][tar] = notpic+pic;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
