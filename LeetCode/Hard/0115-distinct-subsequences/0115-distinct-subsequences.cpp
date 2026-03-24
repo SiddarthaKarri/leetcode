a0fccd7aa348b534bb8ac69cc296e6fc565d2096
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-24 13:29:33 +0530

    Time: 23 ms (76.26%), Space: 43.4 MB (45.7%) - LeetHub

--- LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -34,10 +34,10 @@ public:
         // return dp[0][0];
 
         // optimal
-        vector<long long> dp(m+1,0);
+        vector<unsigned long long> dp(m+1,0);
         dp[m] = 1;
         for(int i=n-1;i>=0;i--){
-            vector<long long> curr = dp;
+            vector<unsigned long long> curr = dp;
             for(int j=m-1;j>=0;j--)
                 if(s[i]==t[j])
                     curr[j] = dp[j+1]+dp[j];
