38dc88493edd78fff7010954444e61fdf4a8ce79
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-24 13:24:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    int memoo(int i,int j,string &s,string &t,vector<vector<int>> &dp) {
-        if(j==t.size()) 
-            return 1;
-        if(i==s.size()) 
-            return 0;
-        if(dp[i][j]!=-1) 
-            return dp[i][j];
-        if(s[i]==t[j]){
-            int take = memoo(i+1,j+1,s,t,dp);
-            int notTake = memoo(i+1,j,s,t,dp);
-            return dp[i][j] = take+notTake;
-        } else
-            return dp[i][j] = memoo(i+1,j,s,t,dp);
-    }
+    // int memoo(int i,int j,string &s,string &t,vector<vector<int>> &dp) {
+    //     if(j==t.size()) 
+    //         return 1;
+    //     if(i==s.size()) 
+    //         return 0;
+    //     if(dp[i][j]!=-1) 
+    //         return dp[i][j];
+    //     if(s[i]==t[j]){
+    //         int take = memoo(i+1,j+1,s,t,dp);
+    //         int notTake = memoo(i+1,j,s,t,dp);
+    //         return dp[i][j] = take+notTake;
+    //     } else
+    //         return dp[i][j] = memoo(i+1,j,s,t,dp);
+    // }
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
-        return memoo(0,0,s,t,dp);
+        // vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
+        // return memoo(0,0,s,t,dp);
+
+        // tabulation
+        int n=s.size(),m=t.size();
+        vector<vector<long long>> dp(n+1,vector<longlong>(m+1,0));
+        for(int i=0;i<=n;i++)
+            dp[i][m] = 1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(s[i]==t[j])
+                    dp[i][j] = dp[i+1][j+1]+dp[i+1][j];
+                else
+                    dp[i][j] = dp[i+1][j];
+            }
+        }
+        return dp[0][0];
     }
 };
