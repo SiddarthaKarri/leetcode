35097d1bc43ebbfe2d98d3a4c2331ab0344306cc
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-24 13:15:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ LeetCode/Hard/1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -15,12 +15,12 @@ public:
         }
         int i=n,j=m;
         string ans = "";
-        while(i>0&&j>0){
+        while(i>0 && j>0){
             if(s[i-1]==t[j-1]){
                 ans += s[i-1];
                 i--;
                 j--;
-            }else if(dp[i-1][j]>dp[i][j-1]){
+            }else if(dp[i-1][j]>=dp[i][j-1]){
                 ans += s[i-1];
                 i--;
             }else{
