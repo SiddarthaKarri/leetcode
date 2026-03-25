b39f9157b7c46489bc940873d03c58cbc4860eee
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-25 14:32:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -17,25 +17,25 @@ public:
         // vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
         // return memo(prices,n,0,0,k,dp);
         // tabu
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        // vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k+1, 0)));
         // opt
-        // vector<vector<int>> ahead(2, vector<int>(3, 0));
-        // vector<vector<int>> cur(2, vector<int>(3, 0));
+        vector<vector<int>> ahead(2, vector<int>(k+1, 0));
+        vector<vector<int>> cur(2, vector<int>(k+1, 0));
         
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 for(int cap=1;cap<=k;cap++){
                     if(buy==0)
-                        dp[ind][buy][cap] = max(dp[ind+1][0][cap], -prices[ind]+dp[ind+1][1][cap]);
-                        // cur[buy][cap] = max(ahead[0][cap],-prices[ind] + ahead[1][cap]);
+                        // dp[ind][buy][cap] = max(dp[ind+1][0][cap], -prices[ind]+dp[ind+1][1][cap]);
+                        cur[buy][cap] = max(ahead[0][cap],-prices[ind] + ahead[1][cap]);
                     if(buy==1)
-                        dp[ind][buy][cap] = max(dp[ind+1][1][cap],prices[ind]+dp[ind+1][0][cap-1]);
-                        // cur[buy][cap] = max(ahead[1][cap], prices[ind] + ahead[0][cap - 1]);
+                        // dp[ind][buy][cap] = max(dp[ind+1][1][cap],prices[ind]+dp[ind+1][0][cap-1]);
+                        cur[buy][cap] = max(ahead[1][cap], prices[ind] + ahead[0][cap - 1]);
                 }
             }
-            // ahead = cur;
+            ahead = cur;
         }
-        return dp[0][0][k];
-        // return cur[0][2];
+        // return dp[0][0][k];
+        return cur[0][k];
     }
 };
