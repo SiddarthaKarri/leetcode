f8a3a36278a7e2d2e3dd476099734b4447fb57d3
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-25 14:15:44 +0530

    Time: 431 ms (30.56%), Space: 223.9 MB (24.13%) - LeetHub

---
+++ LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int memo(vector<int>& Arr, int n, int ind, int buy, int cap,vector<vector<vector<int>>>& dp) {
+        if(ind==n||cap==0)
+            return 0;
+        if(dp[ind][buy][cap]!=-1)
+            return dp[ind][buy][cap];
+        int profit;
+        if(buy==0)
+            profit = max(memo(Arr,n,ind+1,0,cap,dp), -Arr[ind]+memo(Arr,n,ind+1,1,cap,dp));
+        if(buy==1)
+            profit = max(memo(Arr,n,ind+1,1,cap,dp), Arr[ind]+memo(Arr,n,ind+1,0,cap-1,dp));
+        return dp[ind][buy][cap] = profit;
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        return memo(prices,n,0,0,2,dp);
+    }
+};
