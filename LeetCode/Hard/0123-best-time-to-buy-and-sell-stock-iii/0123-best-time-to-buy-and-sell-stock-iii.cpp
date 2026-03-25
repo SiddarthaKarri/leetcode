0e44831b9ce6fd71283f6e0a7c935bf51e1dcf36
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-25 14:28:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -17,17 +17,27 @@ public:
         int n=prices.size();
         // vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
         // return memo(prices,n,0,0,2,dp);
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        // tabu
+        // vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        // opt
+        vector<vector<int>> ahead(2, vector<int>(3, 0));
+        vector<vector<int>> cur(2, vector<int>(3, 0));
+        
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 for(int cap=1;cap<=2;cap++){
                     if(buy==0)
-                        dp[ind][buy][cap] = max(dp[ind+1][0][cap], -prices[ind]+dp[ind+1][1][cap]);
+                        // dp[ind][buy][cap] = max(dp[ind+1][0][cap], -prices[ind]+dp[ind+1][1][cap]);
+                        cur[buy][cap] = max(ahead[0][cap],-prices[ind] + ahead[1][cap]);
                     if(buy==1)
-                        dp[ind][buy][cap] = max(dp[ind+1][1][cap],prices[ind]+dp[ind+1][0][cap-1]);
+                        // dp[ind][buy][cap] = max(dp[ind+1][1][cap],prices[ind]+dp[ind+1][0][cap-1]);
+                        cur[buy][cap] = max(ahead[1][cap], prices[ind] + ahead[0][cap - 1]);
                 }
             }
+            ahead = cur;
         }
-        return dp[0][0][2];
+        // return dp[0][0][2];
+        return cur[n];
     }
 };
