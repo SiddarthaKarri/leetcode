5252ef8188a64a27c63ec830fe16ebd044c9185e
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-25 09:16:11 +0530

    Time: 10 ms (24.53%), Space: 24.7 MB (24.12%) - LeetHub

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -22,14 +22,15 @@ public:
         dp[n][0]=0;
         dp[n][1]=0;
         long pft=0;
-        for(int ind=n-1;ind>=0;i--){
+        for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 if(buy==0)
                     pft = max(dp[ind+1][0], -prices[ind]+dp[ind+1][1]);
                 if(buy==1)
                     pft = max(dp[ind+1][1], prices[ind]+dp[ind+1][0]);
+                dp[ind][buy]=pft;
             }
         }
-        return pft;
+        return dp[0][0];
     }
 };
