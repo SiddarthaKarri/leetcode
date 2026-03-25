15ef69bfae3b87beeb4cf18a4111c30267386d89
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2026-03-25 14:22:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    int memo(vector<int>& Arr, int n, int ind, int buy, int cap,vector<vector<vector<int>>>& dp) {
-        if(ind==n||cap==0)
-            return 0;
-        if(dp[ind][buy][cap]!=-1)
-            return dp[ind][buy][cap];
-        int profit;
-        if(buy==0)
-            profit = max(memo(Arr,n,ind+1,0,cap,dp), -Arr[ind]+memo(Arr,n,ind+1,1,cap,dp));
-        if(buy==1)
-            profit = max(memo(Arr,n,ind+1,1,cap,dp), Arr[ind]+memo(Arr,n,ind+1,0,cap-1,dp));
-        return dp[ind][buy][cap] = profit;
-    }
-    
+    // int memo(vector<int>& Arr,int n,int ind,int buy,int cap,vector<vector<vector<int>>>& dp){
+    //     if(ind==n||cap==0)
+    //         return 0;
+    //     if(dp[ind][buy][cap]!=-1)
+    //         return dp[ind][buy][cap];
+    //     int profit;
+    //     if(buy==0)
+    //         profit = max(memo(Arr,n,ind+1,0,cap,dp), -Arr[ind]+memo(Arr,n,ind+1,1,cap,dp));
+    //     if(buy==1)
+    //         profit = max(memo(Arr,n,ind+1,1,cap,dp), Arr[ind]+memo(Arr,n,ind+1,0,cap-1,dp));
+    //     return dp[ind][buy][cap] = profit;
+    // }
+
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return memo(prices,n,0,0,2,dp);
+        // vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        // return memo(prices,n,0,0,2,dp);
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                    if(buy==0)
+                        dp[ind][buy][cap] = max(dp[ind+1][0][cap], -Arr[ind]+dp[ind+1][1][cap]);
+                    if(buy==1)
+                        dp[ind][buy][cap] = max(dp[ind+1][1][cap],Arr[ind]+dp[ind+1][0][cap-1]);
+                }
+            }
+        }
+        return dp[0][0][2];
     }
 };
